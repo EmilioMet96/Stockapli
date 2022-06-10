d453ce05c8d061f7b051dceed96f09461c9e83c8
EmilioMet96 <emilio.rodriguezor@uanl.edu.mx>
2022-06-10 13:27:19 -0500

    Sacamos el nodemon

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
         console.log('Peticion recibida')
-        res.send('<h1>Hola mundo!, con nodemon</h1>')
+        res.send('Hola mundo!')
     })
 app.listen(4000,()=>{
     console.log('Servidor escuchando desde el puerto 4000')
