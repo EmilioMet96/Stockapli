14f4294f503483d726543fe13c8479c6551f97e7
EmilioMet96 <emilio.rodriguezor@uanl.edu.mx>
2022-06-11 15:22:48 -0500

    Ya se pueden subir los registros a la BD

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
+const { timeStamp } = require('console')
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const path=require('path')
 
 require('dotenv').config()
-app.use(express.static(path.join(__dirname,'public')))
-//app.get('/', (req, res) => {
-  //      console.log('Peticion recibida')
-    //    res.send('Hola mundo!')
-    //})
-    const PORT = process.env.PORT || 4000
-app.listen(PORT,()=>{
+mongoose.connect(`mongodb+srv://EmilioMet:${process.env.MONGO_DB_PASS}@desarrollo.wbgam0f.mongodb.net/stockapp?retryWrites=true&w=majority`)
+.then((result)=>{
+    app.listen(PORT,()=>{
     console.log(`Servidor escuchando desde el puerto ${PORT}`)
+
 } )
+console.log('Conexion exitosa BD')
+})
+.catch((err)=>console.log(err))
+const productSchema =mongoose.Schema({
+    name:{type:String,required:true},
+    price:Number
+},{timestamps :true})
+const Product=mongoose.model('Product',productSchema)
+
+
+app.use(express.static(path.join(__dirname,'public')))
+app.use(express.json())
+app.post('/api/v1/products', (req, res, next) => {
+    const newProduct=new Product(req.body
+       
+
+    )
+    newProduct.
+    save().
+    then((result)=>{
+        res.status(201).json({ok : true})
+})
+.catch((err)=>console.log(err))
+    
+
+    })
+    const PORT = process.env.PORT || 4000
--- public/index.js
+++ public/index.js
@@ -1,1 +1,15 @@
-console.log("Esta consola ya es en el navegador")
+const inputName =document.querySelector('#ProductName')
+const inputPrice =document.querySelector('#ProductPrice')
+
+const button = document.querySelector('button')
+button.addEventListener('click',(e)=>{
+    
+    const name = inputName.value
+    const price = inputPrice.value
+    fetch('/api/v1/products',{method:'POST',
+    headers:{'Content-type':'application/json'},
+    body:
+   JSON.stringify({
+        name,price
+    }),
+})})
