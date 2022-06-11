2eee8dc655fe9fcf0a321012eaa4627a5981debe
EmilioMet96 <emilio.rodriguezor@uanl.edu.mx>
2022-06-11 10:51:34 -0500

    Agregando nuevas cosas

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
+const path=require('path')
+
 require('dotenv').config()
-app.get('/', (req, res) => {
-        console.log('Peticion recibida')
-        res.send('Hola mundo!')
-    })
+app.use(express.static(path.join(__dirname,'public')))
+//app.get('/', (req, res) => {
+  //      console.log('Peticion recibida')
+    //    res.send('Hola mundo!')
+    //})
     const PORT = process.env.PORT || 4000
 app.listen(PORT,()=>{
     console.log(`Servidor escuchando desde el puerto ${PORT}`)
